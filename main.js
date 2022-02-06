a92611baa68970388b45d94d283c8b5a339e04b6
sreeharshalaghuvarapu <72685506+sreeharshalaghuvarapu@users.noreply.github.com>
2022-02-06 18:46:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,13 +11,40 @@ function setup() {
 
     video = createCapture(VIDEO)
     video.hide();
+
+    posenet = ml5.poseNet(video, modelLoded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoded() {
+    console.log('poseNet is initialized');
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
 }
 
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        leftWristX = results[0].pose.leftWrist.X;
+        leftWristY = results[0].pose.leftWrist.y; 
+        console.log("leftWristX = " + leftWristX + "leftWristY ="+ leftWristY)
+
+        rightWristX = results[0].pose.rightWrist.X;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY ="+ rightWristY)
+    }
+     
+}
+
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);                     
+
 }
+
+
