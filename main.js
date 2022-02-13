d875529628c896c8bad24ffa31f4fe264362c69e
sreeharshalaghuvarapu <72685506+sreeharshalaghuvarapu@users.noreply.github.com>
2022-02-13 18:51:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 song = "";
 
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
 function preload()
 {
     song = loadSound("music.mp3")
@@ -22,12 +30,30 @@ function modelLoded() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000")
+    storke("#FF0000")
+
+    if(scoreLeftWrist > 0.2)
+     {
+         circle(leftWristX,leftWristY,20);
+        InNumberleftwristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftwristY)
+        leftWristY_divide_1000 = remove_decimals/1000;
+        volume =leftWristY_divide_1000 *2 ;
+        document.getElementById("volume").innerHTML = "volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 
 function gotPoses(results)
 {
     if(results.length > 0)
     {
+        console.log(results)
+        scoreLeftWrist = results[0].pose.keypoints[9].score
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        
         leftWristX = results[0].pose.leftWrist.X;
         leftWristY = results[0].pose.leftWrist.y; 
         console.log("leftWristX = " + leftWristX + "leftWristY ="+ leftWristY)
